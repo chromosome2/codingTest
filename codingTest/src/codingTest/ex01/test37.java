e4052be447bac9be0794f75eee612b069156bfb0
chromosome2 <tnwls356@gmail.com>
2023-03-04 16:42:43 +0900

    230304

--- codingTest/src/codingTest/ex01/test37.java
+++ codingTest/src/codingTest/ex01/test37.java
@@ -6,31 +6,32 @@ import java.util.Queue;
 public class test37 {
 
 	public static void main(String[] args) {
-		int bridge_length=100;
-		int weight=100;
-		int[] truck_weights= {10,10,10,10,10,10,10,10,10,10};
+		int bridge_length=3;
+		int weight=10;
+		int[] truck_weights= {1,5,6,8,2,2,2,2,1};
 		int answer=0;
 		
-		Queue<Integer> q=new LinkedList<>();
-		
-		int sum=0;
-		
-		int index=0;
-		while(index!=truck_weights.length) {
-			if(q.size()==bridge_length) {
-				sum-=q.remove();
-			}
-			if(sum+truck_weights[index]>weight) {
-				q.offer(0);
-				answer++;
-			}else {
-				sum+=truck_weights[index];
-				q.offer(truck_weights[index]);
-				answer++;
-				index++;
-			}
-		}
-		answer+=bridge_length;
+		//queue 선언
+        Queue<Integer> q=new LinkedList<>();
+
+        int sum=0;
+        int index=0;
+        
+        while(index!=truck_weights.length) {
+            if(q.size()==bridge_length) { // 다리길이만큼 트럭이 차면
+                sum-=q.remove(); //맨 앞에 있던 트럭 도착지로 빼주기 + sum에서도 제거
+            }
+            if(sum+truck_weights[index]>weight) {
+                q.offer(0); // 다리의 빈 공간에 0을 추가해주기
+                answer++;
+            }else {
+                sum+=truck_weights[index];
+                q.offer(truck_weights[index]);
+                answer++;
+                index++;
+            }
+        }
+        answer+=bridge_length;
 		
 		System.out.println(answer);
 
